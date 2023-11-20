5fd37fbc9d0cdc075bd70c8c6ef8bd7b38416dc1
Stedman <michaelstedman81@gmail.com>
2023-11-19 20:43:57 -0600

    Reverted back to local DB

--- server/db.js
+++ server/db.js
@@ -2,19 +2,19 @@ const Pool = require("pg").Pool;
 require('dotenv').config()
 
 const pool = new Pool({
-  // user: process.env.LOCAL_DATABASE_USER,
-  // password: process.env.LOCAL_DATABASE_PASSWORD,
-  // host: process.env.LOCAL_DATABASE_HOST,
-  // port: process.env.LOCAL_DATATBASE_PORT,
-  // database: process.env.LOCAL_DATABASE_NAME,
+  user: process.env.LOCAL_DATABASE_USER,
+  password: process.env.LOCAL_DATABASE_PASSWORD,
+  host: process.env.LOCAL_DATABASE_HOST,
+  port: process.env.LOCAL_DATATBASE_PORT,
+  database: process.env.LOCAL_DATABASE_NAME,
 
-  connectionString: process.env.RENDER_DATABASE_CONNECTION_STRING,
-  ssl: { rejectUnauthorized: false },
-  user: process.env.RENDER_DATABASE_USER,
-  password: process.env.RENDER_DATABASE_PASSWORD,
-  host: process.env.RENDER_DATABASE_HOST,
-  port: process.env.RENDER_DATABASE_PORT,
-  database: process.env.RENDER_DATABASE_NAME,
+  // connectionString: process.env.RENDER_DATABASE_CONNECTION_STRING,
+  // ssl: { rejectUnauthorized: false },
+  // user: process.env.RENDER_DATABASE_USER,
+  // password: process.env.RENDER_DATABASE_PASSWORD,
+  // host: process.env.RENDER_DATABASE_HOST,
+  // port: process.env.RENDER_DATABASE_PORT,
+  // database: process.env.RENDER_DATABASE_NAME,
 });
 
 module.exports = pool;
