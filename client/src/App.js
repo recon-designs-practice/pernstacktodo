07291c5e7904b5667019ac07f94dbd88555f6835
Stedman <michaelstedman81@gmail.com>
2023-11-19 19:51:57 -0600

    Refactored some things I forgot.

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,83 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { TextField } from '@mui/material'
+import axios from 'axios'
 import useTodoStore from './stores/store';
 import Button from './components/Button';
 import './App.css';
 
+const api = "http://localhost:5550/api/todos"
+
 function App() {
   const todosArr = useTodoStore((state) => state.todos)
+  const setTodos = useTodoStore((state) => state.setTodos)
   const newTitle = useTodoStore((state) => state.newTitleInputValue)
   const setNewTitle = useTodoStore((state) => state.setNewTitle)
   const newDescription = useTodoStore((state) => state.newDescriptionInputValue)
   const setNewDescription = useTodoStore((state) => state.setNewDescription)
 
+  useEffect(() => {
+    axios.get(api)
+      .then((response) => {
+        const tempArr = []
+
+        response.data.forEach((task) => {
+          tempArr.push(task)
+        })
+
+        setTodos(tempArr)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [setTodos])
+
+  const handleAddTodo = () => {
+    axios
+      .post(`${api}`, {
+        todo_title: newTitle,
+        todo_description: newDescription,
+        todo_is_complete: false
+      })
+      .then((response) => {
+        axios
+          .get(api)
+          .then((response) => {
+            const tempArr = []
+
+            response.data.forEach((entry) => {
+              tempArr.push(entry)
+            })
+            setTodos(tempArr)
+            setNewTitle('')
+            setNewDescription('')
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
     <div className='grid-container'>
-      <h1>Hello world</h1>
-      <div style={{ gridColumn: '3 / 11' }}>
-        <h6>{newTitle}</h6>
-        <h6>{newDescription}</h6>
-      </div>
-
+      <h1>PostgreSQL Todo List</h1>
       <div className='add-section'>
         <TextField placeholder='Title' variant='outlined' value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
         <TextField placeholder='Description' variant='outlined' value={newDescription} onChange={(e) => setNewDescription(e.target.value)} />
-        <Button className='form-button' label="Add" />
+        <Button className='form-button' label="Add" onClick={handleAddTodo} />
       </div>
       <div style={{ gridColumn: '3 / 11' }}>
-        {todosArr.map((todo) => {
-          const { todo_id, todo_title } = todo
+        {todosArr.map((todo, idx) => {
+          const { todo_title, todo_description } = todo
 
-          return <p key={todo_id}>{todo_title}</p>
+          return (
+            <div key={idx} style={{ margin: '0px 0px 20px 0px' }}>
+              <p style={{ margin: '0px 0px 8px 0px', fontWeight: 'bold' }}>{`${todo_title}`}</p>
+              <p style={{ margin: '0px' }}>{`${todo_description}`}</p>
+            </div>
+          )
         })}
       </div>
     </div>
--- client/src/stores/store.js
+++ client/src/stores/store.js
@@ -1,25 +1,8 @@
 import { createWithEqualityFn } from "zustand/traditional"
 
 const useTodoStore = createWithEqualityFn((set) => ({
-  todos: [
-    {
-      todo_id: '1',
-      todo_title: 'Title 1',
-      todo_description: 'Description 1',
-      todo_is_complete: false,
-      todo_created_on: '10-22-1971'
-    },
-    {
-      todo_id: '2',
-      todo_title: 'Title 2',
-      todo_description: 'Description 2',
-      todo_is_complete: true,
-      todo_created_on: '10-22-1981'
-    }
-  ],
-  addTodo: (newTodo) => set((state) => ({
-    todos: [...state.todos, newTodo]
-  })),
+  todos: [],
+  setTodos: (stuffToAdd) => set(() => ({ todos: [...stuffToAdd] })),
   newTitleInputValue: '',
   setNewTitle: (newTitle) => set({ newTitleInputValue: newTitle }),
   newDescriptionInputValue: '',
